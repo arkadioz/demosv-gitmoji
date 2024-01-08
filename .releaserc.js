f103269e726fc76c72d91b317adc6147f5da167b
arkadioz <davv913@hotmail.com>
2024-01-08 17:45:10 -0500

    :zap: add another format function

--- .releaserc.js
+++ .releaserc.js
@@ -43,6 +43,9 @@ module.exports = {
                   },
                   split: function(string) {
                      return string.trim().split('\n');
+                  },
+                  formatDateCol: function(date) {
+                   return date.toLocaleString('en-US', { timeZone: 'America/New_York', year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' }).replace(',', '');
                   }
                },
                issueResolution: {
