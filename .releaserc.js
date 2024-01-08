2aee28ea83e82f30512142461989a24178ab62a9
unknown <davv913@hotmail.com>
2024-01-07 21:41:09 -0500

    :bug: fix the message for the git plugin

--- .releaserc.js
+++ .releaserc.js
@@ -1,75 +1,83 @@
-// en ".releaserc.js" o "release.config.js"
 const path = require('path');
-const { promisify } = require('util');
+const {
+   promisify
+} = require('util');
 const dateFormat = require('dateformat');
 const readFileAsync = promisify(require('fs').readFile);
 
-// los archivos *.hbs deben pasarse como cadenas de contenido
+// the *.hbs template and partials should be passed as strings of contents
 const template = readFileAsync(path.join(__dirname, '/lib/assets/templates/default-template.hbs'));
 const commitTemplate = readFileAsync(path.join(__dirname, '/lib/assets/templates/commit-template.hbs'));
 
 module.exports = {
-  branches: [
-    "main",
-    {name: 'cookie', prerelease: true},
-  ],
-  plugins: [
-    [
-      'semantic-release-gitmoji', {
-        releaseRules: {
-          major: [ ':boom:' ],
-          minor: [ ':sparkles:' ],
-          patch: [
-            ':bug:',
-            ':ambulance:',
-            ':lock:',
-            ':zap:',
-            ':hammer:'
-          ]
-        },
-        releaseNotes: {
-          template,
-          partials: { commitTemplate },
-          helpers: {
-            datetime: function (format = 'UTC:yyyy-mm-dd') {
-              return dateFormat(new Date(), format);
-            }
-          },
-          issueResolution: {
-            template: '{baseUrl}/{owner}/{repo}/issues/{ref}',
-            baseUrl: 'https://github.com',
-            source: 'github.com',
-            removeFromCommit: false,
-            regex: /#\d+/g
-          }
-        }
-      }
-    ],
-    '@semantic-release/github',
-    [
-      "@semantic-release/exec",
+   branches: [
+      "main",
       {
-        "prepareCmd": "mvn versions:set -DnewVersion=\"${nextRelease.version}\" && mvn clean install"
-      }
-    ],
-    [
-          "@semantic-release/changelog",
-          {
+         name: 'cookie',
+         prerelease: true
+      },
+   ],
+   plugins: [
+      [
+         'semantic-release-gitmoji', {
+            releaseRules: {
+               major: [':boom:'],
+               minor: [':sparkles:'],
+               patch: [
+                  ':bug:',
+                  ':ambulance:',
+                  ':lock:',
+                  ':zap:',
+                  ':hammer:'
+               ]
+            },
+            releaseNotes: {
+               template,
+               partials: {
+                  commitTemplate
+               },
+               helpers: {
+                  datetime: function (format = 'UTC:yyyy-mm-dd') {
+                     return dateFormat(new Date(), format);
+                  }
+               },
+               issueResolution: {
+                  template: '{baseUrl}/{owner}/{repo}/issues/{ref}',
+                  baseUrl: 'https://github.com',
+                  source: 'github.com',
+                  removeFromCommit: false,
+                  regex: /#\d+/g
+               }
+            }
+         }
+      ],
+      '@semantic-release/github',
+      [
+         "@semantic-release/exec",
+         {
+            "prepareCmd": "mvn versions:set -DnewVersion=\"${nextRelease.version}\" && mvn clean install"
+         }
+      ],
+      [
+         "@semantic-release/changelog",
+         {
             "changelogFile": "docs/CHANGELOG.md",
             "changelogTitle": "# :balloon: Gitmoji Changelog :lollipop:"
-          }
-    ],
-    [
-      '@semantic-release/git',
-      {
-        assets: [
-                "**/pom.xml",
-                "docs/CHANGELOG.md"
-              ],
-        message: [
-          ':bookmark: v${nextRelease.version} [skip ci]'
-        ]
-      }
-    ]
-  ]
+         }
+      ],
+      [
+         '@semantic-release/git',
+         {
+            assets: [
+               "**/pom.xml",
+               "docs/CHANGELOG.md"
+            ],
+            message: [
+               ':bookmark: v${nextRelease.version} [skip ci]',
+               '',
+               'https://github.com/arkadioz/demosv-gitmoji/releases/tag/${nextRelease.gitTag}'
+            ].join('\n')
+         }
+      ]
+   ]
 };
